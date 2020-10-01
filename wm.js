1d67f49a61b0b96acac3e38ecc208b177e59d604
mayupenguin <71986705+mayupenguin@users.noreply.github.com>
2020-10-01 15:23:39 -0300

    Create wm.js

---
+++ wm.js
@@ -0,0 +1,1 @@
+Quando um novo membro entra no servidor, o comando deve ser acionado automaticamente enviando uma imagem no servidor, tornando mais fácil para os membros verem quem ingressou no servidor
